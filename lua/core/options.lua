b62764d4c121f2c1b678a00b9faa08db400b1bef
Lee <eldestory@gmail.com>
2023-10-08 17:46:05 +0100

    Added whichkey

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,6 +12,7 @@ vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
+vim.opt.number = true
 
 -- use spaces for tabs and whatnot
 vim.opt.tabstop = 2
